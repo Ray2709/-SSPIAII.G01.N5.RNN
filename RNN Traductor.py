5f9c809b3bedb16e45cd2b4324602683195757de
alonso963341 <jesus.mfranco@alumnos.udg.mx>
2023-05-09 15:43:15 -0600

    división del train y el test eliminación de caracteres especiales, conversión del texto a minúsculas

--- RNN Traductor.py
+++ RNN Traductor.py
@@ -28,3 +28,25 @@ with open(text_file, encoding="utf-8") as f:
 
 import random
 print(random.choice(text_pairs))
+
+random.shuffle(text_pairs)
+num_val_samples=int(0.15*len(text_pairs))
+num_train_samples=len(text_pairs)-2*num_val_samples
+train_pairs=text_pairs[:num_train_samples]
+val_pairs=text_pairs[num_train_samples:num_train_samples+num_val_samples]
+test_pairs=text_pairs[num_train_samples+num_val_samples:]
+
+strip_chars = string.punctuation + "¿"
+strip_chars = strip_chars.replace("[","")
+strip_chars = strip_chars.replace("]","")
+
+# 
+def custom_standardization(input_string):
+    lowercase = tf.strings.lower(input_string)
+    return tf.strings.regex_replace(
+        lowercase, f"[{re.escape(strip_chars)}]",""
+    )
+
+# 
+vocab_size = 15000
+sequence_length = 20
